c767e119e4dee49cb4d950150135f9830306a241
RobertBChung <rbchung@hkcoche.org>
2025-04-02 17:37:56 +0800

    fix to move to hand bug and flip card

--- PlayerHand.gd
+++ PlayerHand.gd
@@ -13,12 +13,10 @@ var deck = ["one", "two", "three", "four"]
 func _ready():
 	centre_screen_x = get_viewport().size.x / 2
 
-
 func startGame():
 	var card = preload(card_scene)
 	for i in range(hand_size):
 		var randomNumber = randi() % (deck.size())
-		print(randomNumber)
 		var new_card = card.instantiate()
 		new_card.name = deck[randomNumber]
 		var cardImagePath = str("res://Cards/" + new_card.name + ".png")
@@ -27,20 +25,19 @@ func startGame():
 		deck.erase(randomNumber)
 		$"../CardManager".add_child(new_card)
 		add_card_to_hand(new_card)	
+		new_card.get_node("AnimationPlayer").play("flip")
 		
 func add_card_to_hand(card):
 	if card not in playerHand:
 		playerHand.insert(0, card)
 		update_hand_position()
-	else:
-		animation_card_to_position(card, card.startingPosition)
 	
 func update_hand_position():
 	for i in range(playerHand.size()):
 		var new_position = Vector2(calculateHandPosition(i), hand_y_position)
 		var card = playerHand[i]
 		card.startingPosition = new_position
-		animation_card_to_position(card, new_position)
+		animation_card_to_position(card, card.startingPosition)
 		
 func calculateHandPosition(index):
 	var totalWidth = (playerHand.size() - 1) * card_width
@@ -51,12 +48,10 @@ func animation_card_to_position(card, new_position):
 	card.position = $"../Deck".position
 	var tween = get_tree().create_tween()
 	tween.tween_property(card, "position", new_position, 0.3)
-	pass
 	
 func remove_card_from_hand(card):
 	if card in playerHand:
 		playerHand.erase(card)
-		update_hand_position()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
